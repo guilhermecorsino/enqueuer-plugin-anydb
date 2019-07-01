f9272435b3fd631f030fe16e59676f71e17acc46
= <=>
2019-07-01 20:51:47 -0300

    cleans up the project with needless files

--- src/my-parser.ts
+++
@@ -1,9 +0,0 @@
-import {ObjectParser} from 'enqueuer';
-
-export class MyParser implements ObjectParser {
-
-    parse(value: string, query?: any): object {
-        return JSON.parse(value);
-    }
-
-}
