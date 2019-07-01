38e736a63caac573b83bca714bbeddf3038fc1f1
guilhermecorsino <guilhermecorsino@outlook.com>
2019-07-01 20:51:47 -0300

    cleans up the project with needless files

--- src/my-formatter.ts
+++
@@ -1,9 +0,0 @@
-import {ReportFormatter, OutputRequisitionModel as RequisitionModel} from 'enqueuer';
-
-export class MyReportFormatter implements ReportFormatter {
-
-    public format(report: RequisitionModel): string {
-        return JSON.stringify(report, null, 2);
-    }
-
-}
