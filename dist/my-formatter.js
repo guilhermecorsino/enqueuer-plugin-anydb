693a5e77c8244913fbd4da2a539eb240e5fbcfc4
= <=>
2019-07-01 23:02:14 -0300

    Create enqueuer self tests

--- dist/my-formatter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MyReportFormatter {
-    format(report) {
-        return JSON.stringify(report, null, 2);
-    }
-}
-exports.MyReportFormatter = MyReportFormatter;
